7df325809b30b36bd76182bfe462640b6cc8cb8d
NourElGafy <nour-elgafy@outlook.com>
2020-09-22 14:37:50 +0200

    Added INotify PropertyChanged Implementation in the BaseModel File

--- BaseTemplate/BaseTemplate/Models/BaseModel.cs
+++ BaseTemplate/BaseTemplate/Models/BaseModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BaseTemplate.Models
 {
-    public abstract class BaseModel
+    public abstract class BaseModel : INotifyPropertyChanged
     {
         //The id is set to int just for the simplicity of the demo
         public int Id { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
