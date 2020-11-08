5b1547e4d5da0435912ec165cf6bdbebb97008cb
Ahmed Hussein <pharaohfury1337@gmail.com>
2020-11-08 13:33:59 +0200

    add view model resolver unit testing

---
+++ TemplateFoundationUnitTest/Navigation/Implementations/ViewModelResolverData.cs
@@ -0,0 +1,111 @@
+﻿using BaseTemplate;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TemplateFoundation.ViewModelFoundation;
+
+namespace TemplateFoundationUnitTest.Navigation.Implementations
+{
+	public class ViewModelResolverData : IEnumerable<object[]>
+	{
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			// get all type contains viewmodel postfix
+			IEnumerable<Type> viewModelAssemblyTypes = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel"));
+			// filter types to get only classes
+			IEnumerable<Type> viewModelImplementations = viewModelAssemblyTypes.Where(a => a.IsClass);
+			foreach (Type type in viewModelImplementations)
+			{
+				BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
+				yield return new object[] { type, "Test data", viewModel };
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+
+	public class ViewModelResolverWithoutViewModelData : IEnumerable<object[]>
+	{
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			// get all type contains viewmodel postfix
+			IEnumerable<Type> viewModelAssemblyTypes = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel"));
+			// filter types to get only classes
+			IEnumerable<Type> viewModelImplementations = viewModelAssemblyTypes.Where(a => a.IsClass);
+			foreach (Type type in viewModelImplementations)
+			{
+				yield return new object[] { type, "Test data" };
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+	public class ViewModelResolverWithoutTypeData : IEnumerable<object[]>
+	{
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			// get all type contains viewmodel postfix
+			IEnumerable<Type> viewModelAssemblyTypes = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel"));
+			// filter types to get only classes
+			IEnumerable<Type> viewModelImplementations = viewModelAssemblyTypes.Where(a => a.IsClass);
+			foreach (Type type in viewModelImplementations)
+			{
+				BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
+				yield return new object[] { "Test data", viewModel };
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+
+	public class ViewModelResolverGenericWithoutDataData : IEnumerable<object[]>
+	{
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			// get all type contains viewmodel postfix
+			IEnumerable<Type> viewModelAssemblyTypes = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel"));
+			// filter types to get only classes
+			IEnumerable<Type> viewModelImplementations = viewModelAssemblyTypes.Where(a => a.IsClass);
+			foreach (Type type in viewModelImplementations)
+			{
+				yield return new object[] { type };
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+
+	public class ViewModelResolverGenericData : IEnumerable<object[]>
+	{
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			// get all type contains viewmodel postfix
+			IEnumerable<Type> viewModelAssemblyTypes = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel"));
+			// filter types to get only classes
+			IEnumerable<Type> viewModelImplementations = viewModelAssemblyTypes.Where(a => a.IsClass);
+			foreach (Type type in viewModelImplementations)
+			{
+				yield return new object[] { type, "Test data" };
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ TemplateFoundationUnitTest/Navigation/Implementations/ViewModelResolverUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TemplateFoundation.Navigation.Implementations;
+using TemplateFoundation.ViewModelFoundation;
+using Xamarin.Forms;
+using Xunit;
+
+namespace TemplateFoundationUnitTest.Navigation.Implementations
+{
+	public class ViewModelResolverUnitTest
+	{
+		[Theory]
+		[ClassData(typeof(ViewModelResolverData))]
+		public void ResolveViewModelUnitTest(Type type, object data, BaseViewModel baseViewModel)
+		{
+			Page page = ViewModelResolver.ResolveViewModel(type, data, baseViewModel);
+			Assert.NotNull(page);
+		}
+
+		[Theory]
+		[ClassData(typeof(ViewModelResolverWithoutViewModelData))]
+		public void ResolveViewModelWithoutViewModelUnitTest(Type type, object data)
+		{
+			Page page = ViewModelResolver.ResolveViewModel(type, data);
+			Assert.NotNull(page);
+		}
+
+		[Theory]
+		[ClassData(typeof(ViewModelResolverWithoutTypeData))]
+		public void ResolveViewModelWithoutTypeUnitTest(object data, BaseViewModel baseViewModel)
+		{
+			Page page = ViewModelResolver.ResolveViewModel(data, baseViewModel);
+			Assert.NotNull(page);
+		}
+
+		[Theory]
+		[ClassData(typeof(ViewModelResolverGenericWithoutDataData))]
+		public void ResolveViewModelGenericWithoutDataUnitTest(Type type)
+		{
+			Page page = (Page)typeof(ViewModelResolver).GetMethod(nameof(ViewModelResolver.ResolveViewModel), new Type[0]).MakeGenericMethod(type).Invoke(this, null);
+			Assert.NotNull(page);
+		}
+
+		[Theory]
+		[ClassData(typeof(ViewModelResolverGenericData))]
+		public void ResolveViewModelGenericaUnitTest(Type type, object data)
+		{
+			object[] parameters = new object[] { data };
+			Page page = (Page)typeof(ViewModelResolver).GetMethod(nameof(ViewModelResolver.ResolveViewModel), new Type[] { typeof(object) }).MakeGenericMethod(type).Invoke(this, parameters);
+			Assert.NotNull(page);
+		}
+	}
+}
