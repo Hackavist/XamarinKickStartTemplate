15bf156961f56d7bc9816d1d1f83c0a909c991b8
NourElGafy <Nour-ElGafy@outlook.com>
2020-11-09 11:47:38 +0200

    fixed test refrences errors and updated nuget and formated view model tests

--- TemplateFoundationUnitTest/Navigation/Implementations/ViewModelResolverData.cs
+++ TemplateFoundationUnitTest/Navigation/Implementations/ViewModelResolverData.cs
@@ -8,76 +8,57 @@ using TemplateFoundation.ViewModelFoundation;
 
 namespace TemplateFoundationUnitTest.Navigation.Implementations
 {
-	public class ViewModelResolverData : IEnumerable<object[]>
-	{
-		IEnumerator IEnumerable.GetEnumerator()=>return GetEnumerator();
-		
-		public IEnumerator<object[]> GetEnumerator()
-		{
-			// get all type contains viewmodel postfix where type is class
-			IEnumerable<Type> viewModelImplementations = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel")).Where(a => a.IsClass);
-			foreach (Type type in viewModelImplementations)
-			{
-				BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
-				yield return new object[] { type, "Test data", viewModel };
-			}
-		}		
-	}
+    public class ViewModelResolverTestBase : IEnumerable<object[]>
+    {
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-	public class ViewModelResolverWithoutViewModelData : IEnumerable<object[]>
-	{
-		IEnumerator IEnumerable.GetEnumerator()=>return GetEnumerator();
 		public IEnumerator<object[]> GetEnumerator()
 		{
 			// get all type contains viewmodel postfix where type is class
 			IEnumerable<Type> viewModelImplementations = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel")).Where(a => a.IsClass);
-			foreach (Type type in viewModelImplementations)
-			{
-				yield return new object[] { type, "Test data" };
+            foreach (Type type in viewModelImplementations)
+            {
+				yield return GetData(type);
 			}
-		}
-	}
-	public class ViewModelResolverWithoutTypeData : IEnumerable<object[]>
-	{
-		IEnumerator IEnumerable.GetEnumerator()=>return GetEnumerator();
-		public IEnumerator<object[]> GetEnumerator()
-		{
-			// get all type contains viewmodel postfix where type is class
-			IEnumerable<Type> viewModelImplementations = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel")).Where(a => a.IsClass);
-			foreach (Type type in viewModelImplementations)
-			{
-				BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
-				yield return new object[] { "Test data", viewModel };
-			}
+        }
+
+		public virtual object[] GetData(Type type)
+        {
+			BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
+			return new object[] { type, "Test data", viewModel };
 		}
 	}
 
-	public class ViewModelResolverGenericWithoutDataData : IEnumerable<object[]>
+	public class ViewModelResolverWithoutViewModelData : ViewModelResolverTestBase, IEnumerable<object[]>
 	{
-		IEnumerator IEnumerable.GetEnumerator()=>return GetEnumerator();
-		public IEnumerator<object[]> GetEnumerator()
+		public override object[] GetData(Type type)
 		{
-			// get all type contains viewmodel postfix where type is class
-			IEnumerable<Type> viewModelImplementations = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel")).Where(a => a.IsClass);
-			foreach (Type type in viewModelImplementations)
-			{
-				yield return new object[] { type };
-			}
+			return new object[] { type, "Test data" };
 		}
-
 	}
 
-	public class ViewModelResolverGenericData : IEnumerable<object[]>
+	public class ViewModelResolverWithoutTypeData : ViewModelResolverTestBase, IEnumerable<object[]>
 	{
-		IEnumerator IEnumerable.GetEnumerator()=>return GetEnumerator();
-		public IEnumerator<object[]> GetEnumerator()
+		public override object[] GetData(Type type)
 		{
-			// get all type contains viewmodel postfix where type is class
-			IEnumerable<Type> viewModelImplementations = Assembly.Load(typeof(App).Assembly.GetName()).ExportedTypes.Where(a => a.Name.ToLower().EndsWith("viewmodel")).Where(a => a.IsClass);
-			foreach (Type type in viewModelImplementations)
-			{
-				yield return new object[] { type, "Test data" };
-			}
+			BaseViewModel viewModel = (BaseViewModel)Activator.CreateInstance(type);
+			return new object[] { "Test data", viewModel };
+		}
+	}
+
+	public class ViewModelResolverGenericWithoutDataData : ViewModelResolverTestBase, IEnumerable<object[]>
+	{
+		public override object[] GetData(Type type)
+		{
+			return new object[] { type };
+		}
+	}
+
+	public class ViewModelResolverGenericData : ViewModelResolverTestBase, IEnumerable<object[]>
+	{
+		public override object[] GetData(Type type)
+		{
+			return new object[] { type, "Test data" };
 		}
 	}
 }
